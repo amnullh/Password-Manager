1bbfa2e6ec02e233499cb9c9ec6da604edf84a3c
Md Amanullah <amn3242@gmail.com>
2024-02-21 17:19:28 +0530

    Images added

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ window.title("Password Manager")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=200)
-lock_img = PhotoImage(file="logo.png")
+lock_img = PhotoImage(file="images/logo.png")
 canvas.create_image(100, 100, image=lock_img)
 canvas.grid(column=1, row=0)
 
